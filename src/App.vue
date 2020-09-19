6b90942d73c6753d096075212660e9b9247ccca7
Daniel <daniel.torresg@udea.edu.co>
2020-09-19 09:56:39 -0500

    Vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+   <v-app>
+    <Toolbar />
+    <v-main>
+       <Search v-on:searchResult="searchResult" />
+      <ImageList v-bind:items="items" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Toolbar from "./components/Toolbar";
+import Search from "./components/Search";
+import ImageList from "./components/ImageList";
+
+
+
+export default {
+  name: 'App',
+
+  components: {
+    Toolbar,
+    Search,
+    ImageList,
+  },
+  methods: {    
+       searchResult(response){
+         this.itmes = response;
+         console.log(this.itmes);
+
+       }
+
+  },
+
+  data: () => ({
+    items: []
+    //
+  }),
+};
+</script>
---
+++ src/components/ImageList.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-row>
+      <v-col v-bind:key="item.id" v-for="item in items.hits" sm="4" cols="12">
+                <v-card>
+                    <v-card-title>
+                                                
+                    </v-card-title>
+
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    
+</template>
+
+<script>
+export default {
+    name:'ImageList',
+    props:[
+        "items"
+    ]
+}
+</script>
